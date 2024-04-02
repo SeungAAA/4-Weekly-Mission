7bd5ad615e02c2488ee1a8caa60749430ef8f438
SeungAAA <155128712+SeungAAA@users.noreply.github.com>
2024-04-02 15:42:52 +0900

    [feat] : next config 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  images: {
+    domains: ['codeit-front.s3.ap-northeast-2.amazonaws.com'],
+  },
+};
